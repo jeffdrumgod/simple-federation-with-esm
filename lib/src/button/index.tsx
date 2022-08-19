45fb29d286e08eb930aa9a179a4144ca06133752
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2022-08-19 14:58:55 -0300

    fix less css module

--- lib/src/button/index.tsx
+++ lib/src/button/index.tsx
@@ -1,11 +1,8 @@
-import React, { version } from "react";
 import Button, { ButtonProps } from "antd/es/button";
 // import "./style.less";
 import style from "./style.module.less";
 // import "./style.css";
 // import "./style.css.js";
-
-console.log("🚀 ~ file: button/index.tsx ~ line 8 ~ version", version);
 
 const Btn = (props: ButtonProps) => {
   const { children, ...extrProps } = props;
